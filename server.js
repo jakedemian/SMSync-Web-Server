ba2bbaff260285ee9aed551b6e70484f19aa443e
jakedemianrosetta <Jacob.Demian@rosetta.com>
2016-12-13 16:36:31 -0500

    made edit to code to fix error

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ function relayMessage(request, params){
 // Create server and listen on PORT
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server = http.createServer(handleRequest);
 server.listen(server_port, server_ip_address, 511, function () {
 
     logOnServer( "Listening on " + server_ip_address + ", server_port " + server_port );
